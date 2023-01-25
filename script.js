220f2960848182c7170139bdaadd00cfd3d30654
Czomila <kamila_romanik@student.uw.edu.pl>
2023-01-25 20:03:30 +0100

    Ble

---
+++ script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    const element = document.getElementById("id01");
+    element.innerHTML = "New Heading";
+}
+
+function myColorChange() {
+    const element = document.getElementById("id01");
+    element.style.color = "red";
+}
+
+
+
+
+function mojaFunkcja(){
+//ciało funkcji
+
+}
